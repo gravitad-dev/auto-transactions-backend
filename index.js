03533a56e4b8ed7a4e39a3d3ee054a66fc95e60c
silvak <silvak.jeg@gmail.com>
2024-07-08 06:03:01 -0400

    fix: CORS problem

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const logger = require("morgan")
 
 //------------------------ CONFIG SERVER -------------------------------
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
